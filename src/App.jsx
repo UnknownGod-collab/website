9eeb54f35498514a313714dac7c4cd5b699de710
SkillerSoul <>
2025-03-30 20:45:01 +0200

    route website

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router';
-import Home from './pages/Home';
-import About from './pages/About';
-import Game from './pages/Game';
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Game from "./pages/Game";
 
 function App() {
   return (
     <>
     <Router>
       <Routes>
-        <Route path="" element={<Home />} />
+        <Route path="/website" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/game" element={<Game />} />
       </Routes>
